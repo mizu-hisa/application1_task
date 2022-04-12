46c41f3716346a17b33f1e1cc243acddd2a5855e
mizu-hisa <101858095+mizu-hisa@users.noreply.github.com>
2022-04-12 09:16:29 +0000

    [app_confih]ページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,37 @@
 class BooksController < ApplicationController
-  def new
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)  
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
   end
 
   def index
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
-  def edit
+  
+  
+  
+   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+   end
+  
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
+  
 end
+
